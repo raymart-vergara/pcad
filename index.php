8342fb35f1621992653f99a60f89792d584dc11d
jirish18 <irishbautista1801@gmail.com>
2024-02-06 10:23:15 +0800

    ty

--- index.php
+++ index.php
@@ -344,22 +344,22 @@
             </div>
         </footer>
 
-        <!-- jQuery -->
+        <!-- jQuery
         <script src="plugins/jquery/dist/jquery.min.js"></script>
         <!-- jQuery UI 1.11.4 -->
-        <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+        <!-- <script src="plugins/jquery-ui/jquery-ui.min.js"></script> -->
         <!-- SweetAlert2 -->
-        <script type="text/javascript" src="plugins/sweetalert2/dist/sweetalert2.min.js"></script>
+        <!-- <script type="text/javascript" src="plugins/sweetalert2/dist/sweetalert2.min.js"></script> -->
         <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-        <script>
+        <!-- <script>
             $.widget.bridge('uibutton', $.ui.button)
-        </script>
+        </script> -->
         <!-- Bootstrap 4 -->
-        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
         <!-- overlayScrollbars -->
-        <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+        <!-- <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
         <!-- AdminLTE App -->
-        <script src="dist/js/adminlte.js"></script>
+        <!-- <script src="dist/js/adminlte.js"></script>  -->
 
 </body>
 
