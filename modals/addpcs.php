f5e4e5e3570bb21e8eca20b990c549747b094b26
alymrnn <allym.furukawa@gmail.com>
2024-04-12 15:26:59 +0800

    changed final process into ip address column

--- modals/addpcs.php
+++ modals/addpcs.php
@@ -1,5 +1,6 @@
-<div class="modal fade bd-example-modal-xl" id="new_pcs" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+<div class="modal fade bd-example-modal-xl" id="new_pcs" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">
@@ -24,11 +25,11 @@
             <input type="text" id="andon_line_master" class="form-control" autocomplete="off">
           </div>
         </div>
-        <div class="row">
+        <div class="row mt-4">
           <div class="col-4">
-            <label for="final_process_master">Final Process</label>
+            <label for="final_process_master">IP Address Column</label>
             <select id="final_process_master" class="form-control" maxlength="255" autocomplete="off">
-              <option value="">......</option> 
+              <option value="">......</option>
             </select>
           </div>
           <div class="col-4">
--- pcs_page/admin/plugins/footer.php
+++ pcs_page/admin/plugins/footer.php
@@ -1,5 +1,5 @@
  <footer class="main-footer">
-    <strong>Copyright &copy; 2024. Developed by: Jane Irish Bautista</strong>
+    <strong>Copyright &copy; 2024. Developed by: Jane Irish Bautista & AKM</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
